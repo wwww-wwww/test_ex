c20f9819a5db2003072c7f6698d3fbc1697f966a
w <wvvwvvvvwvvw@gmail.com>
2021-10-11 11:23:38 -0700

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :test, TestWeb.Endpoint,
   # Possibly not needed, but doesn't hurt
   http: [port: {:system, "PORT"}],
   # url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
-  url: [host: "embed.ml"],
+  url: [host: "embed.moe"],
   #force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   server: true
